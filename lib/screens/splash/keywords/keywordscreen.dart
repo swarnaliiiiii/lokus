a6cde1afb49fea1ec3d5636132dc4bb175bf6ff6
swarnaliiiiii <oli2k3@gmail.com>
2025-05-13 21:15:37 +0530

    refactor: update keyword color and implement custom navigation bar in keywords screen

--- lib/screens/splash/keywords/keywordscreen.dart
+++ lib/screens/splash/keywords/keywordscreen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lokus/domains/constants/appcolors.dart';
+import 'package:lokus/screens/splash/splashscreen.dart';
+import 'package:lokus/widgets/nav_bar.dart';
 
-class Keywordscreen extends StatefulWidget{
+class Keywordscreen extends StatefulWidget {
   const Keywordscreen({Key? key}) : super(key: key);
 
   @override
@@ -13,42 +15,34 @@ class _KeywordscreenState extends State<Keywordscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.keywordColor,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              color: AppColors.dashColor,
-
+        backgroundColor: AppColors.keywordColor,
+        body: Column(
+          children: [
+            CustomNav(
+              icon: Icons.arrow_back,
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SplashScreen()));
+              },
+              // Removed invalid 'child' parameter
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              color: AppColors.page2Color,
-              
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text("What's your \n budget?",
+                  style: GoogleFonts.manrope(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w900,
+                    color: const Color.fromARGB(255, 23, 70, 109),
+                  ),
+                ),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              color: AppColors.page3Color,
-              
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 200,
-              color: AppColors.splashColor,
-            ),
-          ),
-        ],
-      )
-    );
+          ],
+        ));
   }
 }
---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CustomNav extends StatelessWidget {
+  final VoidCallback? onTap;
+  final VoidCallback? onSkip;
+  final IconData icon;
+  final String label;
+
+  const CustomNav({
+    Key? key,
+    this.onTap,
+    this.onSkip,
+    this.icon = Icons.arrow_back,
+    this.label = "Next",
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 35.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // Skip button
+          GestureDetector(
+            onTap: onTap,
+            child: Row(
+              children: [
+                const SizedBox(width: 8),
+                Icon(
+                  icon,
+                  color: const Color.fromARGB(255, 23, 70, 109),
+                  size: 28,
+                ),
+              ],
+            ),
+          ),
+          GestureDetector(
+            onTap: onSkip,
+            child: const Text(
+              "Skip",
+              style: TextStyle(
+                color: Color.fromARGB(255, 23, 70, 109),
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

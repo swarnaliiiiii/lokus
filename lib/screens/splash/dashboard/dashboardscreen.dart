21f2fa80a52c7330d400bfdf23c94e3599e1c2b0
swarnaliiiiii <oli2k3@gmail.com>
2025-05-23 21:58:41 +0530

    feat: add Dashboardscreen with basic layout and welcome message

---
+++ lib/screens/splash/dashboard/dashboardscreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Dashboardscreen extends StatefulWidget{
+  const Dashboardscreen({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboardscreen> createState() => _DashboardscreenState();
+}
+
+class _DashboardscreenState extends State<Dashboardscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Dashboard!'),
+      ),
+    );
+  }
+}

5bad0e5286ae7bb59119af483302c9838cc08a30
swarnaliiiiii <oli2k3@gmail.com>
2025-05-29 12:44:03 +0530

    feat: add dotenv and flutter_gemini dependencies; initialize Gemini with API key from environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lokus/app.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
+  String apiKey = dotenv.env['API_KEY'] ?? '';
+  Gemini.init(apiKey: apiKey);
   runApp(const MyApp());
 }

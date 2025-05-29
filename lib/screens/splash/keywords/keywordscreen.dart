bb869d6e9f67ce43192bafdf8ad05682d3409e21
swarnaliiiiii <oli2k3@gmail.com>
2025-05-29 13:14:59 +0530

    feat: implement budget selection feature in Keywordscreen; add HTTP request for budget submission and update Square widget for tap functionality

--- lib/screens/splash/keywords/keywordscreen.dart
+++ lib/screens/splash/keywords/keywordscreen.dart
@@ -7,6 +7,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lokus/widgets/square.dart';
 import 'package:lokus/screens/splash/keywords/keywordscreen2.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Keywordscreen extends StatefulWidget {
   const Keywordscreen({Key? key}) : super(key: key);
@@ -14,21 +16,50 @@ class Keywordscreen extends StatefulWidget {
   @override
   State<Keywordscreen> createState() => _KeywordscreenState();
 }
+
 class _KeywordscreenState extends State<Keywordscreen> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => Keywordscreen2()));
-    });
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Timer(Duration(seconds: 3), () {
+  //     Navigator.pushReplacement(
+  //         context, MaterialPageRoute(builder: (context) => Keywordscreen2()));
+  //   });
+  // }
+
   final List<String> _budgetOptions = [
     'Budget',
     'Moderate',
     'Comfortable',
     'Premium',
   ];
+
+  String? _selectedBudget;
+
+  Future<void> _selectBudget(String budget) async {
+    final url = Uri.parse('https://api.example.com/select-budget');
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'budget': budget}),
+    );
+    print('Response status: ${response.statusCode}');
+  }
+
+  void onOptionTap(String budget) {
+    setState(() {
+      _selectedBudget = budget;
+    });
+    _selectBudget(budget).then((_) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Keywordscreen2(),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +100,7 @@ class _KeywordscreenState extends State<Keywordscreen> {
               itemBuilder: (context, index) {
                 return Square(
                   child: _budgetOptions[index],
+                  onTap: () => onOptionTap(_budgetOptions[index]),
                 );
               },
             ),
--- lib/widgets/square.dart
+++ lib/widgets/square.dart
@@ -3,30 +3,37 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lokus/domains/constants/appcolors.dart';
 
 class Square extends StatelessWidget {
-  final child;
+  final String child; // Fixed: Added proper type annotation
+  final VoidCallback? onTap; // Fixed: Added missing onTap parameter
 
-  Square({Key? key, this.child}) : super(key: key);
-  
+  const Square({Key? key, required this.child, this.onTap}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 100.h,
-        width: 200.w,
-        decoration: BoxDecoration(
-          color: AppColors.page2Color,
-          borderRadius: BorderRadius.circular(20.r),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-          child: Text(
-            child,
-            style: TextStyle(
-              fontSize: 23.sp,
-              fontWeight: FontWeight.w900,
-              color: const Color.fromARGB(255, 23, 70, 109),
-            )
+      child: GestureDetector( // Fixed: Added tap functionality
+        onTap: onTap,
+        child: Container(
+          height: 100.h,
+          width: 200.w,
+          decoration: BoxDecoration(
+            color: AppColors.page2Color,
+            borderRadius: BorderRadius.circular(20.r),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            child: Center( // Fixed: Added Center widget for better text alignment
+              child: Text(
+                child,
+                style: TextStyle(
+                  fontSize: 23.sp,
+                  fontWeight: FontWeight.w900,
+                  color: const Color.fromARGB(255, 23, 70, 109),
+                ),
+                textAlign: TextAlign.center, // Fixed: Added text alignment
+              ),
+            ),
           ),
         ),
       ),

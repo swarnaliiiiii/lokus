9ff8ebb4313a930bcc8c836ecb54fd75389dbd63
swarnaliiiiii <oli2k3@gmail.com>
2025-06-03 17:45:37 +0530

    feat: redesign Dashboardscreen layout for improved user experience

--- lib/screens/splash/dashboard/dashboardscreen.dart
+++ lib/screens/splash/dashboard/dashboardscreen.dart
@@ -3,8 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lokus/screens/search_prop/searchbox.dart' as custom_search;
 import 'package:google_fonts/google_fonts.dart';
 
-
-
 class Dashboardscreen extends StatefulWidget {
   const Dashboardscreen({Key? key}) : super(key: key);
 
@@ -18,29 +16,68 @@ class _DashboardscreenState extends State<Dashboardscreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dashboard'),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Get Ready for an Amazing Trip!',
-                style: GoogleFonts.manrope(
-                  fontSize: 24.sp,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header section
+            SizedBox(height: 20.h),
+            Text(
+              'Get Ready for an',
+              style: GoogleFonts.manrope(
+                fontSize: 28.sp,
+                fontWeight: FontWeight.w300,
+                color: Colors.black87,
+              ),
+            ),
+            Text(
+              'Amazing Trip!',
+              style: GoogleFonts.manrope(
+                fontSize: 28.sp,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            
+            SizedBox(height: 30.h),
+            
+            // Search section
+            custom_search.SearchBar(),
+            
+            SizedBox(height: 40.h),
+            
+            // Explore section
+            Text(
+              'Explore Destinations',
+              style: GoogleFonts.manrope(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
+              ),
+            ),
+            
+            SizedBox(height: 20.h),
+            
+            // You can add more content here like destination cards, etc.
+            Expanded(
+              child: Container(
+                // Placeholder for destination content
+                child: Center(
+                  child: Text(
+                    'Destination cards will go here',
+                    style: GoogleFonts.manrope(
+                      fontSize: 16.sp,
+                      color: Colors.grey[600],
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 20.h),
-              custom_search.SearchBar(),
-              SizedBox(height: 20.h),
-              Text(
-                'Explore Destinations',
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
